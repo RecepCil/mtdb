5cfe681ae0f907766af79e36e8f5224f20a2775a
Recep Çil <recepcil95@hotmail.com>
2020-10-08 22:03:15 +0300

    TMDB API Connection & List Movies

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./App.css";
-import Movies from "./components/Movies";
+import MovieManagement from "./components/MovieManagement";
+import Row from "./components/Row";
+import requests from "./requests";
 
 function App() {
   return (
-    <div className="App">
-      <div className="col-md-8 offset-md-2">
-        <Movies />
-      </div>
+    <div className="app">
+      {false ? (
+        <div className="col-md-8 offset-md-2">
+          <MovieManagement />
+        </div>
+      ) : (
+        <>
+          <Row
+            title="Netflix Originals"
+            fetchUrl={requests.fetchNetflixOriginals}
+          />
+          <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+          <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        </>
+      )}
     </div>
   );
 }
